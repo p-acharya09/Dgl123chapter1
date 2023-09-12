dc17501931fe8d0e4f8ac29ed6f559fbdce18eb6
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-09-11 17:20:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$name = "Guest";
-$name = "Prashant";
-$name = "Jack";
-$price = 2;
+$nutrition = [
+    "fat" => 42,
+    "sugar" => 60,
+    "salt" => 3.5,
+    "protein" => 2.6
+]
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +15,10 @@ $price = 2;
 </head>
 <body>
     <h1>The Candy Store</h1>
-    <h2>Welcome <?php echo $name; ?></h2>
-    <p> The cost of your candy is $<?php echo $price; ?> per pack. </p>
+    <h2>Nutrition (per 100g)</h2>
+    <p>Fat: <?php echo $nutrition["fat"]; ?> </p>
+    <p>Sugar: <?php echo $nutrition["sugar"]; ?> </p>
+    <p>Salt: <?php echo $nutrition["salt"]; ?> </p>
+    <p>Protein: <?php echo $nutrition["protein"]; ?> </p>
 </body>
 </html>
