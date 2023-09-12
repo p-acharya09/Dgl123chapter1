0ba0b60d43f85e509607155eacb9f61f416ab204
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-09-11 18:26:01 -0700

    String concatenation

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php
-$items = 15;
-$cost = 6;
-$subtotal = $cost * $items;
-$tax = ($subtotal/100) * 20;
-$total = $subtotal + $tax;
+$prefix = "Thank you";
+$name = "Ivy";
+$message = $prefix .", ". $name; 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Arithmetic Operators</title>
+    <title>String Operators</title>
 </head>
 <body>
     <h1>The Candy Store</h1>
-    <h2>Shopping Cart</h2>
-    <p>Items:<?= $items ?></p>
-    <p>Cost per pack:<?= $cost ?></p>
-    <p>Subtotal:<?= $subtotal ?></p>
-    <p>Tax:<?= $tax ?></p>
-    <p>Total:<?= $total ?></p>
+    <h2><?= $name ?>'s Order</h2>
+    <p><?= $message ?></p>
 
 </body>
 </html>
