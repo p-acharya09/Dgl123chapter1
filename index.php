1e282f616fbbb4112bafa2a44ec4035397cc2e4c
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-09-11 18:19:40 -0700

    Arithmetic operations in php

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
-$name = "Prashant Acharya";
-$favourites = ['Snickers','Chocolate', 'Toffee', 'Fudge'];
+$items = 15;
+$cost = 6;
+$subtotal = $cost * $items;
+$tax = ($subtotal/100) * 20;
+$total = $subtotal + $tax;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Echo Shorthand</title>
+    <title>Arithmetic Operators</title>
 </head>
 <body>
     <h1>The Candy Store</h1>
-    <h2>Welcome <?= $name ?></h2>
-    <p>Your favourite type of candy is:
-        <?= $favourites[0] ?>.
-    </p>
+    <h2>Shopping Cart</h2>
+    <p>Items:<?= $items ?></p>
+    <p>Cost per pack:<?= $cost ?></p>
+    <p>Subtotal:<?= $subtotal ?></p>
+    <p>Tax:<?= $tax ?></p>
+    <p>Total:<?= $total ?></p>
+
 </body>
 </html>
