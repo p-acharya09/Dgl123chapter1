1fd7ef63829818d27a3201d0e8ae95552d7717c7
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-09-11 17:34:38 -0700

    Creating and Accessing Indexed Arrays

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$nutrition = [
-    "fat" => 42,
-    "sugar" => 60,
-    "salt" => 3.5,
-    "protein" => 2.6
-]
+$best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Licorice', 'Bubble gum', 'Toffee', 'Jelly beans'];
 ?>
 
 <!DOCTYPE html>
@@ -15,10 +10,13 @@ $nutrition = [
 </head>
 <body>
     <h1>The Candy Store</h1>
-    <h2>Nutrition (per 100g)</h2>
-    <p>Fat: <?php echo $nutrition["fat"]; ?> </p>
-    <p>Sugar: <?php echo $nutrition["sugar"]; ?> </p>
-    <p>Salt: <?php echo $nutrition["salt"]; ?> </p>
-    <p>Protein: <?php echo $nutrition["protein"]; ?> </p>
+    <h2>Best Sellers</h2>
+    <ul>
+        <li><?php echo $best_sellers[0]; ?></li>
+        <li><?php echo $best_sellers[1]; ?></li>
+        <li><?php echo $best_sellers[2]; ?></li>
+        <li><?php echo $best_sellers[3]; ?></li>
+        <li><?php echo $best_sellers[4]; ?></li>
+    </ul>
 </body>
 </html>
