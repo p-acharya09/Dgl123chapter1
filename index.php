816c7ac2d2899c63c571c77c3606529fb68bc4c0
p-acharya09 <56748540+p-acharya09@users.noreply.github.com>
2023-09-11 18:53:24 -0700

    Complete Example exercise

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
-$item = "Chocolate";
-$stock = 3;
-$wanted = 5;
-$deliver = true;
-$can_buy = (($wanted <= $stock)&&($deliver == true));
+$username = "Prashant";
+$greeting = "Hi, $username.";
+$offer = [
+    'item' => 'Chocolate',
+    'qty' => 3,
+    'price' => 6,
+    'discount' => 4,
+];
+$usual_price = $offer['qty'] * $offer['price'];
+$offer_price = $offer['qty'] * $offer['discount'];
+$saving = $usual_price - $offer_price;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Comparison operator</title>
+    <title>The Candy Store</title>
 </head>
 <body>
     <h1>The Candy Store</h1>
-    <h2>Shopping Cart</h2>
-    <p>Item: <?= $item ?></p>
-    <p>Stock: <?= $stock ?></p>
-    <p>Ordered: <?= $wanted ?></p>
-    <p>Can buy: <?= $can_buy ?></p>
-
+    <h2>Multi-buy Offer</h2>
+    <p><?= $greeting ?></p>
+    <p>Save $<?= $saving ?></p>
+    <p>Buy <?= $offer['qty']?> packs of <?=$offer['item']?>
+    for $<?= $offer_price ?><br>(Usual price $<?=$usual_price?>)</p>
 </body>
 </html>
